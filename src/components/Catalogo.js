e7cb24ddc99fb9b96e219136ff5d0fbeb9450aec
PaulERC <paulrc.06@gmail.com>
2023-10-17 17:03:23 -0600

    Change env variable name

--- src/components/Catalogo.js
+++ src/components/Catalogo.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import Producto from './Producto'
 
-const url=process.env.REACT_APP_API_KEY;
+const url=process.env.REACT_APP_API_URL;
 
 const Catalogo = () => {
 
